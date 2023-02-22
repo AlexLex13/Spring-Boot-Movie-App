b4215d3222789266b7a4ba1c49fe267ea9cca79b
Alexey <suxovarov.alexey@gmail.com>
2023-02-22 10:14:55 +0300

    Initial commit

---
+++ src/main/java/dev/alex/fstack/FStackApplication.java
@@ -0,0 +1,13 @@
+package dev.alex.fstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FStackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/alex/fstack/FStackApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alex.fstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
