5cb14db0b57fece3ac4761371017a7df01deadb6
Alexey <suxovarov.alexey@gmail.com>
2023-02-22 11:29:50 +0300

    add MovieService

--- src/main/java/dev/alex/fstack/servise/MovieService.java
+++ src/main/java/dev/alex/fstack/servise/MovieService.java
@@ -1,11 +1,17 @@
 package dev.alex.fstack.servise;
 
+import dev.alex.fstack.model.Movie;
+import dev.alex.fstack.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class MovieService {
-
     @Autowired
-    private
+    private MovieRepository movieRepository;
+    private List<Movie> allMovies(){
+        return movieRepository.findAll();
+    }
 }
