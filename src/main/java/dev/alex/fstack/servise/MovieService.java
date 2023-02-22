62553ce907368406cf36b1b339e0ef8e91fdff07
Alexey <suxovarov.alexey@gmail.com>
2023-02-22 11:33:55 +0300

    add getAllMovies

--- src/main/java/dev/alex/fstack/servise/MovieService.java
+++ src/main/java/dev/alex/fstack/servise/MovieService.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class MovieService {
     @Autowired
     private MovieRepository movieRepository;
-    private List<Movie> allMovies(){
+    public List<Movie> allMovies(){
         return movieRepository.findAll();
     }
 }
