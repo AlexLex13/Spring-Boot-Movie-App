5661147b451bbfe1344f9bf7f641e793e808383d
Alexey <suxovarov.alexey@gmail.com>
2023-02-22 12:31:17 +0300

    add getSingleMovie

--- src/main/java/dev/alex/fstack/controller/MovieController.java
+++ src/main/java/dev/alex/fstack/controller/MovieController.java
@@ -2,14 +2,17 @@ package dev.alex.fstack.controller;
 
 import dev.alex.fstack.model.Movie;
 import dev.alex.fstack.servise.MovieService;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -20,5 +23,10 @@ public class MovieController {
     @GetMapping
     public ResponseEntity<List<Movie>> getAllMovies(){
         return new ResponseEntity<>(movieService.allMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable ObjectId id){
+        return new ResponseEntity<>(movieService.singleMovie(id), HttpStatus.OK);
     }
 }
