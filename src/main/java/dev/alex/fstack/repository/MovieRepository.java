b7a450ab4179b0e5da0750318c43c537a0b9ddf4
Alexey <suxovarov.alexey@gmail.com>
2023-02-22 11:27:09 +0300

    add MovieRepository

---
+++ src/main/java/dev/alex/fstack/repository/MovieRepository.java
@@ -0,0 +1,10 @@
+package dev.alex.fstack.repository;
+
+import dev.alex.fstack.model.Movie;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+}
---
+++ src/main/java/dev/alex/fstack/servise/MovieService.java
@@ -0,0 +1,11 @@
+package dev.alex.fstack.servise;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MovieService {
+
+    @Autowired
+    private
+}
